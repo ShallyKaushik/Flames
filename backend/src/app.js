8275c5590c3a10174a0683e03f70e8aebd268275
ShallyKaushik <shallykaushik00@gmail.com>
2026-07-20 13:28:52 +0530

    Setup database configuration and backend foundation

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,31 @@
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import compression from "compression";
+
+import errorHandler from "./middlewares/error.middleware.js";
 
 const app = express();
 
+app.use(cors());
+
+app.use(helmet());
+
+app.use(compression());
+
 app.use(express.json());
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
+
     res.json({
         success: true,
         message: "Flames Backend Running"
     });
+
 });
+
+app.use(errorHandler);
 
 export default app;
